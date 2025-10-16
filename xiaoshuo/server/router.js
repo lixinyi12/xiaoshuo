298919a06fba7985d8984db3b941f18b78eca2cd
lemon <2256628892@qq.com>
2025-10-16 22:52:32 +0800

    add utils

--- xiaoshuo/server/router.js
+++ xiaoshuo/server/router.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-var validator = require('validator.tool')
+var validator = require('validator')
 // 检查 value 是否为一个空对象，集合，映射或者set
 var isEmpty = require('lodash/isEmpty')
 
@@ -13,18 +13,18 @@ const validatorInput = (data) => {
     if(validator.isEmpty(data.password)){
         errors.password = "密码不能为空"
     }
-    if(validator.isEmail(data.email)){
+    if(!validator.isEmail(data.email)){
         errors.email = "不符合邮箱格式"
     }
     if(validator.isEmpty(data.phone)){
         errors.phone = "电话号码不能为空"
     }
-    if(validator.equals(data.password,data.password2)){
+    if(!validator.equals(data.password,data.password2)){
         errors.password2 = "两次密码不相同"
     }
 
     return{
-        isvalid:!isEmpty(errors),
+        isValid:!isEmpty(errors),
         errors
     }
 }
@@ -37,7 +37,7 @@ router.post('/register',(req,res)=>{
     const password2= req.body.password2;
     const {isValid,errors} = validatorInput(req.body)
     if(isValid){
-        res.status(400).json(errors)
+        res.send(errors)
     }else{
         res.send({
             msg:"success"
---
+++ xiaoshuo/src/utils/request.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import { error } from 'console';
+import { promises } from 'dns';
+import { response } from 'express';
+import { config } from 'process';
+import qs from 'querystring'
+
+const errorHandle = (status,info) =>{
+    switch (status) {
+        case 400:
+            console.log('语义有误，当前请求无法被服务器理解')
+            break;
+        case 401:
+            console.log('服务器认证失败')
+            break;
+        case 403:
+            console.log('服务器理解请求，但拒绝执行它')
+            break;
+        case 404:
+            console.log('请检查网络请求地址')
+            break;
+        case 500:
+            console.log('服务器遇到未曾预料的状况，导致无法完成对请求的处理')
+            break;
+        case 502:
+            console.log('作为网关或代理工作的服务器尝试执行结束时，从上游服务器接收到无效请求')
+            break;
+        default:
+            break;
+    }
+}
+
+const instance = axios.create({
+    baseURL:'http://iwenwiki.com',
+    timeout:5000
+})
+
+instance.interceptors.request.use(
+    config => {
+        if(config.method === 'post'){
+            config.data = qs.stringify(config.data)
+        }
+        return config
+    },
+    error => Promise.reject(error)
+)
+
+instance.interceptors.response.use(
+    response => response.status === 200 ? Promise.resolve(response) : Promise.reject,
+    error => {
+        const {response} = error
+        errorHandle(response.status,response.info)
+    }
+)
+
+export default instance
